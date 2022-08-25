2ffb3ff7488b9484344e7e17f7e440a3f24ec84d
Priyanshu Sharma <sharmapriyanshu594@gmail.com>
2022-08-25 21:23:58 +0530

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 const http = require('http');
 
+const todo = ['apple is a fruit','an apple a day keeps the doctor away']
+
 http.createServer((req,res)=>{
-res.writeHead(200, {"Content-Type": "Text/html"});
-res.write('<h1>Welcome! to my server</h1>');
-res.end();
+    const {method,url} = req;
+
+    if(url === '/todolist'){
+        if(method === 'GET'){
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        res.write(todo.toString());
+        }
+        else if(method === 'POST'){
+            let body = "";
+            req
+            .on('data',(chunk)=>{
+                body += chunk;
+                
+            })
+            .on('error',(err)=>{
+                console.log("error");
+            })
+            .on('end',()=>{
+                body=JSON.parse(body);
+                console.log("data:",body);
+            });
+        }
+
+    }
+    else{
+        res.writeHead(404);
+    }
+
+    res.end();
 }).listen(8080,()=>{
     console.log("Server is Running")
 });
